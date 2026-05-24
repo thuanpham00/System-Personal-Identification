98c74720bc09ff3fdb1e898dfe13ee2bd509a750
Pham Thuan <phamminhthuan912@gmail.com>
2026-05-24 22:59:53 +0700

    fix client

--- cccd-verify/main.py
+++ cccd-verify/main.py
@@ -28,9 +28,14 @@ app = FastAPI(
     version="6.0.0"
 )
 
+origins = [
+    "http://localhost:3000",
+    "https://system-personal-identification.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
